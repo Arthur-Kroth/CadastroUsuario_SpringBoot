6150fed0a63d86f92474c7c231b7c644b5e9f683
Arthur Kroth Posselt <akposselt@gmail.com>
2026-04-10 20:34:56 -0300

    feat: add methods findByEmail() and deleteByEmail()

--- src/main/java/com/cadastro_usuario/infrastructure/repository/UserRepository.java
+++ src/main/java/com/cadastro_usuario/infrastructure/repository/UserRepository.java
@@ -1,9 +1,15 @@
 package com.cadastro_usuario.infrastructure.repository;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.transaction.annotation.Transactional;
 import com.cadastro_usuario.infrastructure.entity.User;
 
+
 public interface UserRepository extends JpaRepository<User, Integer> {
-    
+
+    Optional<User> findByEmail(String email);
+
+    @Transactional
+    void deleteByEmail(String email);
 }
