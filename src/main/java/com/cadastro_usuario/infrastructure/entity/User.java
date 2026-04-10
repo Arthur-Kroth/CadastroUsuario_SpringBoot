7f7127f0b25ac12597abd4c997ba89ba9e0b729d
Arthur Kroth Posselt <akposselt@gmail.com>
2026-04-09 23:28:40 -0300

    feat: add variables from the class User

--- src/main/java/com/cadastro_usuario/infrastructure/entity/User.java
+++ src/main/java/com/cadastro_usuario/infrastructure/entity/User.java
@@ -1,8 +1,9 @@
 package com.cadastro_usuario.infrastructure.entity;
 
-import jakarta.annotation.Generated;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -21,6 +22,12 @@ import lombok.Setter;
 public class User {
 
     @Id
-    @GeneratedValue(Strategy = GenerationType.AUTO)
-    private Long id;
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    @Column(name = "email", unique = true)
+    private String email;
+
+    @Column(name = "name")
+    private String name;
 }
